21acaec701bbc967446c703799f9ae5e4815493c
eunxoo <80334038+eunxoo@users.noreply.github.com>
2021-07-28 17:20:19 +0900

    Update 15881.py

--- 15881.py
+++ 15881.py
@@ -1,13 +1,14 @@
 n = int(input())
 
 ppap = list(input())
-
+ppap.extend([0] * 1000000)
 num , i = 0, 0
 
 while(i < n) :
-    if(ppap[i]) == 'p' and (ppap[i]) == 'P' and (ppap[i]) == 'A' and (ppap[i]) == 'p':
-        num += 1
-        i+=1
+    if(ppap[i]) == 'p' and (ppap[i+1]) == 'P' and (ppap[i+2]) == 'A' and (ppap[i+3]) == 'p':
         ppap[i+3] = 0
+        num += 1
+    i+=1
+        
 
 print(num)
