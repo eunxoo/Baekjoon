9a31f6bd0173449f49979cbac155fa98b197195a
eunxoo <dmstn7989@naver.com>
2022-06-26 01:37:10 +0900

    Create 2562.py

---
+++ python/2562.py
@@ -0,0 +1,15 @@
+num_list = []
+for i in range(9):
+    num_list.append(int(input()))
+
+print(max(num_list))
+print(num_list.index(max(num_list))+1)
+# 3
+# 29
+# 38
+# 12
+# 57
+# 74
+# 40
+# 85
+# 61
