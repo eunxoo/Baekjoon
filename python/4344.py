3f97a1ea862c627f4a3470e7273fe7fb610650aa
eunxoo <dmstn7989@naver.com>
2022-06-28 00:58:51 +0900

    Create 4344.py

---
+++ python/4344.py
@@ -0,0 +1,11 @@
+case_num = int(input())
+for _ in range(case_num):
+    arr = list(map(int, input().split()))
+    n = arr.pop(0)
+    arr_avg = sum(arr) / n
+    per = 0
+    for score in arr:
+        if score > arr_avg :
+            per += 1
+        percent = (per/n*100)
+    print(f'{percent:.3f}%')
